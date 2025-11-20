32b7749277e42363238a5fd8f778aa2d42aef736
elldeeone <73735118+elldeeone@users.noreply.github.com>
2025-11-20 12:55:28 +1100

    add identify observed addr as external

--- tcp-hole-punch/bridge/src/swarm.rs
+++ tcp-hole-punch/bridge/src/swarm.rs
@@ -777,9 +777,7 @@ fn handle_swarm_event(swarm: &mut Swarm<BridgeBehaviour>, event: SwarmEvent<Brid
         SwarmEvent::Behaviour(BridgeBehaviourEvent::Identify(event)) => match event {
             identify::Event::Received { peer_id, ref info, .. } => {
                 info!("[IDENTIFY] received from {}: protocols={:?} addrs={:?}", peer_id, info.protocols, info.listen_addrs);
-                if let Some(addr) = info.observed_addr.as_ref() {
-                    record_observed_addr(&mut swarm, addr);
-                }
+                record_observed_addr(&mut swarm, &info.observed_addr);
                 for addr in &info.listen_addrs {
                     peer_book.record_address(peer_id, addr.clone());
                     record_observed_addr(&mut swarm, addr);
@@ -787,9 +785,7 @@ fn handle_swarm_event(swarm: &mut Swarm<BridgeBehaviour>, event: SwarmEvent<Brid
             }
             identify::Event::Pushed { peer_id, ref info, .. } => {
                 info!("[IDENTIFY] pushed to {}: protocols={:?} addrs={:?}", peer_id, info.protocols, info.listen_addrs);
-                if let Some(addr) = info.observed_addr.as_ref() {
-                    record_observed_addr(&mut swarm, addr);
-                }
+                record_observed_addr(&mut swarm, &info.observed_addr);
                 for addr in &info.listen_addrs {
                     record_observed_addr(&mut swarm, addr);
                 }
