8cd9e65d54a43ddef61793d04957527685edf688
elldeeone <73735118+elldeeone@users.noreply.github.com>
2025-11-20 14:47:49 +1100

    promote observed addr into identify and advertise on confirm

--- tcp-hole-punch/bridge/src/swarm.rs
+++ tcp-hole-punch/bridge/src/swarm.rs
@@ -779,15 +779,15 @@ fn handle_swarm_event(swarm: &mut Swarm<BridgeBehaviour>, event: SwarmEvent<Brid
         }
         SwarmEvent::Behaviour(BridgeBehaviourEvent::Identify(event)) => match event {
             identify::Event::Received { peer_id, ref info, .. } => {
-                info!("[IDENTIFY] received from {}: protocols={:?} addrs={:?}", peer_id, info.protocols, info.listen_addrs);
-                // Only the observed address tells us how the remote sees *us*; listen_addrs belong to the remote.
+                info!("[IDENTIFY] received from {}: protocols={:?} addrs={:?} observed={}", peer_id, info.protocols, info.listen_addrs, info.observed_addr);
+                // Record remote listen addrs for dialing them, and also add our observed addr so the swarm advertises it.
                 record_observed_addr(swarm, &info.observed_addr);
                 for addr in &info.listen_addrs {
                     peer_book.record_address(peer_id, addr.clone());
                 }
             }
             identify::Event::Pushed { peer_id, ref info, .. } => {
-                info!("[IDENTIFY] pushed to {}: protocols={:?} addrs={:?}", peer_id, info.protocols, info.listen_addrs);
+                info!("[IDENTIFY] pushed to {}: protocols={:?} addrs={:?} observed={:?}", peer_id, info.protocols, info.listen_addrs, info.observed_addr);
             }
             other => {
                 debug!("[IDENTIFY] behaviour event event={:?}", other);
